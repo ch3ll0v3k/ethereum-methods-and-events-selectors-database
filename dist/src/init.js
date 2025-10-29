213b756e0420336166d6b3033f666f62003413da
Timoschenko V <ch3ll0v3k@yandex.com>
2025-10-29 14:39:27 +0100

    sync

--- dist/src/init.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./interfaces/prototypes/console");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.default = {};
